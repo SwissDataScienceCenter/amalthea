8687261039f345129aa62b9e68ae84a4a35e3483
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-04-21 00:51:23 +0200

    feat: several improvements and refactoring
    - fix operator helm chart
    - route all calls through traefik even without oidc enabled
    - enable rescheduling on node failure (experimental)

--- controller/src/server_controller.py
+++ controller/src/server_controller.py
@@ -2,13 +2,17 @@ import yaml
 import kopf
 import kubernetes.client as k8s_client
 from kubernetes.client.rest import ApiException
+from datetime import datetime, timezone, timedelta
 
-from k8s_resources import get_resource_dicts, get_resource_configs
+from k8s_resources import get_resources_specs, get_resource_configs
+import config
 
 
 def create_namespaced_resource(client, api, method, **kwargs):
-    """Create a k8s resource given and api, the right method for creation
-    and the specs of the resource."""
+    """
+    Create a k8s resource given and api, the right method for creation
+    and the specs of the resource.
+    """
     api_class = getattr(client, api)()
     api_method = getattr(api_class, method)
     try:
@@ -19,25 +23,28 @@ def create_namespaced_resource(client, api, method, **kwargs):
 
 @kopf.on.create("renku.io/v1alpha1", "JupyterServer")
 def create_fn(spec, **kwargs):
-
+    """
+    Watch the creation of jupyter server objects and create all
+    the necessary k8s resources which implement the jupyter server.
+    """
     metadata = kwargs["body"]["metadata"]
     spec = kwargs["body"]["spec"]
 
-    resource_dicts = get_resource_dicts(metadata, spec)
+    resources_specs = get_resources_specs(metadata, spec)
     resource_configs = get_resource_configs(
-        auth_kind=spec["auth"]["kind"], api_only=True
+        oidc_enabled=spec["auth"]["oidc"]["enabled"], api_only=True
     )
 
     resources = []
-    for resource_key, resource_dict in resource_dicts.items():
-        kopf.adopt(resource_dict)
+    for resource_key, resource_spec in resources_specs.items():
+        kopf.adopt(resource_spec)
         resources.append(
             create_namespaced_resource(
                 k8s_client,
                 resource_configs[resource_key]["api"],
                 resource_configs[resource_key]["creation_method"],
                 namespace=metadata["namespace"],
-                body=resource_dict,
+                body=resource_spec,
             )
         )
 
@@ -54,3 +61,44 @@ def create_fn(spec, **kwargs):
         )
 
     return {"children": [resource.metadata.uid for resource in resources]}
+
+
+# Note: This is a very experimental feature and it's implementation is likely
+#       to evolve over time. Use with care.
+if config.reschedule_on_node_failure:
+
+    @kopf.timer(
+        "renku.io/v1alpha1", "JupyterServer", initial_delay=60, interval=15, idle=15
+    )
+    def clean_pods_on_dead_nodes(spec, **kwargs):
+        """
+        Periodically check all jupyter server objects for the health of their host
+        node. Kill pods on unreachable/dead nodes with the sledgehammer. This brings
+        a risk multiple containers writing to the same volume should the pod still
+        be running on the unreachable node.
+        """
+        pod_status = k8s_client.CoreV1Api().read_namespaced_pod_status(
+            namespace=kwargs["body"]["metadata"]["namespace"],
+            name=f"{kwargs['body']['metadata']['name']}-0",
+        )
+        ready_cond = [
+            cond for cond in pod_status.status.conditions if cond.type == "Ready"
+        ][0]
+
+        # Would be nice if this came as a boolean already...
+        if not (ready_cond.status == "False" and pod_status.status.phase == "Running"):
+            print("all good...")
+            return
+
+        status_age = (
+            datetime.now(ready_cond.last_transition_time.tzinfo)
+            - ready_cond.last_transition_time
+        )
+
+        if status_age > timedelta(minutes=1):
+            k8s_client.CoreV1Api().delete_namespaced_pod(
+                namespace=kwargs["body"]["metadata"]["namespace"],
+                name=f"{kwargs['body']['metadata']['name']}-0",
+                grace_period_seconds=0,
+                propagation_policy="Background",
+            )
