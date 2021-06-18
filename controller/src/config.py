0a9b8e2c4a35aadb5f0f2ee4e3541703bea53d09
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-06-18 20:40:56 +0200

    feat: add networkpolicies, refactor labling

--- controller/src/config.py
+++ controller/src/config.py
@@ -1,5 +1,6 @@
-import yaml
+import json
 import os
+import yaml
 
 api_group = "renku.io"
 api_version = "v1alpha1"
@@ -18,3 +19,5 @@ try:
         kopf_operator_settings = yaml.safe_load(f.read())
 except FileNotFoundError:
     kopf_operator_settings = {}
+
+amalthea_selector_labels = yaml.safe_load(os.getenv("AMALTHEA_SELECTOR_LABELS", "{}"))
