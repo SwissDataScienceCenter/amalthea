68b07ab2d084cfeccec1f939c8dac87e658b7702
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-08-22 15:24:25 +0200

    chore: add small README for authorization plugin

--- authorization/app.js
+++ authorization/app.js
@@ -4,14 +4,7 @@ const port = 3000
 
 const userId = process.env.USER_ID;
 
-// Note: This is a really just a poc here which could go in two directions.
-// Either get rid of this completely and let the oauth2 proxy handle it,
-// potentially we'd have to contribute some functionality upstream. Or we
-// see this as our entrypoint for enforcing rules which are configurable through
-// the custom resource spec.
-
 app.get('/', (req, res) => {
-  console.log(req.headers)
   if (req.headers['x-auth-request-user'] === userId) {
     res.sendStatus(200)
   }
