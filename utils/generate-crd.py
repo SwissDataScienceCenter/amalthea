38e80702ed22958758b3a5efccdb15e73ca89baa
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-09-16 12:05:05 +0200

    docs: automatically generate CRD from helm template

---
+++ utils/generate-crd.py
@@ -0,0 +1,26 @@
+import yaml
+import subprocess
+
+repo_root = (
+    subprocess.check_output("git rev-parse --show-toplevel", shell=True)
+    .decode()
+    .strip()
+)
+
+rendered_chart = (
+    subprocess.check_output(
+        f"helm template {repo_root}/helm-chart/amalthea",
+        shell=True,
+    )
+    .decode()
+    .split("---")
+)
+
+for spec in rendered_chart:
+    obj = yaml.safe_load(spec)
+    if obj and obj["kind"] == "CustomResourceDefinition":
+        with open((f"{repo_root}/docs/crd.yaml"), "w") as outfile:
+            outfile.write(
+                "# This CRD is automatically generated gerated from the helm charts, do not modify! \n"
+            )
+            yaml.safe_dump(obj, outfile)
