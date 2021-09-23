b890c69cef463011fbe6ab1ca28ebf87eb05cbd4
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-09-23 10:47:47 +0200

    squashme: add name guard to script

--- utils/render-chart-manifests.py
+++ utils/render-chart-manifests.py
@@ -10,55 +10,61 @@ and removes some helm specific annotations. For convenience, we also
 generate a kustomization file.
 """
 
-repo_root = (
-    subprocess.check_output("git rev-parse --show-toplevel", shell=True)
-    .decode()
-    .strip()
-)
+if __name__ == "__main__":
 
-rendered_chart = subprocess.check_output(
-    f"helm template amalthea -n default {repo_root}/helm-chart/amalthea",
-    shell=True,
-).decode()
+    repo_root = (
+        subprocess.check_output("git rev-parse --show-toplevel", shell=True)
+        .decode()
+        .strip()
+    )
 
-lines = rendered_chart.splitlines()
-filtered_lines = [line for line in lines if "helm.sh/chart:" not in line]
-filtered_lines = [
-    line for line in filtered_lines if "app.kubernetes.io/managed-by: Helm" not in line
-]
-all_manifests = "\n".join(filtered_lines)
-manifests = all_manifests.split("---")
+    rendered_chart = subprocess.check_output(
+        f"helm template amalthea -n default {repo_root}/helm-chart/amalthea",
+        shell=True,
+    ).decode()
 
-kustomization = {
-    "apiVersion": "kustomize.config.k8s.io/v1beta1",
-    "kind": "Kustomization",
-    "resources": [],
-}
+    lines = rendered_chart.splitlines()
+    filtered_lines = [line for line in lines if "helm.sh/chart:" not in line]
+    filtered_lines = [
+        line
+        for line in filtered_lines
+        if "app.kubernetes.io/managed-by: Helm" not in line
+    ]
+    all_manifests = "\n".join(filtered_lines)
+    manifests = all_manifests.split("---")
 
-# empty the manifests directory
-shutil.rmtree(f"{repo_root}/manifests/", ignore_errors=True)
-os.mkdir(f"{repo_root}/manifests/")
+    kustomization = {
+        "apiVersion": "kustomize.config.k8s.io/v1beta1",
+        "kind": "Kustomization",
+        "resources": [],
+    }
 
-for manifest in manifests:
-    manifest = manifest.lstrip("\n")
-    if len(manifest) == 0:
-        continue
-    template_path = manifest.splitlines()[0].split("# Source: amalthea/templates/")[1]
-    out_filename = f"{repo_root}/manifests/{template_path}"
-    try:
-        os.makedirs(os.path.dirname(out_filename))
-    except FileExistsError:
-        pass
-    with open(out_filename, "a+") as outfile:
-        outfile.write("---\n")
-        outfile.write(
-            "# This manifest is auto-generated from the helm chart, do not modify! \n"
-        )
-        outfile.write(manifest)
-    kustomization["resources"].append(f"./{template_path}")
+    # empty the manifests directory
+    shutil.rmtree(f"{repo_root}/manifests/", ignore_errors=True)
+    os.mkdir(f"{repo_root}/manifests/")
 
-# some resources are inside the same file
-kustomization["resources"] = sorted(list(set(kustomization["resources"])))
+    for manifest in manifests:
+        manifest = manifest.lstrip("\n")
+        if len(manifest) == 0:
+            continue
+        template_path = manifest.splitlines()[0].split("# Source: amalthea/templates/")[
+            1
+        ]
+        out_filename = f"{repo_root}/manifests/{template_path}"
+        try:
+            os.makedirs(os.path.dirname(out_filename))
+        except FileExistsError:
+            pass
+        with open(out_filename, "a+") as outfile:
+            outfile.write("---\n")
+            outfile.write(
+                "# This manifest is auto-generated from the helm chart, do not modify! \n"
+            )
+            outfile.write(manifest)
+        kustomization["resources"].append(f"./{template_path}")
 
-with open(f"{repo_root}/manifests/kustomization.yaml", "w") as kustomization_file:
-    yaml.dump(kustomization, kustomization_file)
+    # some resources are inside the same file
+    kustomization["resources"] = sorted(list(set(kustomization["resources"])))
+
+    with open(f"{repo_root}/manifests/kustomization.yaml", "w") as kustomization_file:
+        yaml.dump(kustomization, kustomization_file)
