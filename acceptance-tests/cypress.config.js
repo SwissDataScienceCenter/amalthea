ec6a009c8312a6cf4e61a41070e6d4068b8bea67
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2022-08-10 17:30:44 +0200

    squashme: update setup-python action and cache pipenv

--- acceptance-tests/cypress.config.js
+++ acceptance-tests/cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
