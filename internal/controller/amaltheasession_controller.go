a31b724378d2b3f5a7150169db57f6bf2c3b9be7
Samuel Gaist <samuel.gaist@idiap.ch>
2024-06-26 15:38:35 +0200

    feat(controller): add initial minimal logic to handle CRD

--- internal/controller/amaltheasession_controller.go
+++ internal/controller/amaltheasession_controller.go
@@ -19,6 +19,10 @@ package controller
 import (
 	"context"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -32,6 +36,14 @@ type AmaltheaSessionReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+// Definitions to manage status conditions
+const (
+	// typeAvailableAmaltheaSession represents the status of the StatefulSet reconciliation
+	typeAvailableAmaltheaSession = "Available"
+	// typeDegradedAmaltheaSession represents the status used when the custom resource is deleted and the finalizer operations are must to occur.
+	typeDegradedAmaltheaSession = "Degraded"
+)
 
 //+kubebuilder:rbac:groups=amalthea.dev,resources=amaltheasessions,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=amalthea.dev,resources=amaltheasessions/status,verbs=get;update;patch
@@ -47,9 +59,47 @@ type AmaltheaSessionReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *AmaltheaSessionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	amaltheasession := &amaltheadevv1alpha1.AmaltheaSession{}
+	err := r.Get(ctx, req.NamespacedName, amaltheasession)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// If the custom resource is not found then, it usually means that it was deleted or not created
+			// In this way, we will stop the reconciliation
+			log.Info("amaltheasession resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get amaltheasession")
+		return ctrl.Result{}, err
+	}
+
+	if amaltheasession.Status.Conditions == nil || len(amaltheasession.Status.Conditions) == 0 {
+		meta.SetStatusCondition(&amaltheasession.Status.Conditions, metav1.Condition{Type: typeAvailableAmaltheaSession, Status: metav1.ConditionUnknown, Reason: "Reconciling", Message: "Starting reconciliation"})
+		if err = r.Status().Update(ctx, amaltheasession); err != nil {
+			log.Error(err, "Failed to update amaltheasession status")
+			return ctrl.Result{}, err
+		}
+
+		// Let's re-fetch the amaltheasession Custom Resource after update the status
+		// so that we have the latest state of the resource on the cluster and we will avoid
+		// raise the issue "the object has been modified, please apply
+		// your changes to the latest version and try again" which would re-trigger the reconciliation
+		// if we try to update it again in the following operations
+		if err := r.Get(ctx, req.NamespacedName, amaltheasession); err != nil {
+			log.Error(err, "Failed to re-fetch amaltheasession")
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Check if the AmaltheaSession instance is marked to be deleted, which is
+	// indicated by the deletion timestamp being set.
+	isAmaltheaSessionMarkedToBeDeleted := amaltheasession.GetDeletionTimestamp() != nil
+	if isAmaltheaSessionMarkedToBeDeleted {
+		// Add finalizer handling if needed
+		return ctrl.Result{}, nil
+	}
 
 	return ctrl.Result{}, nil
 }
