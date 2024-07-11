b8f6575a30fa4eb49f3c02c0eb2e49b9b805e189
Salim Kayal <salim.kayal@idiap.ch>
2024-07-11 10:08:12 +0200

    feat: detect need to scale down the StatefulSet

---
+++ internal/controller/cull.go
@@ -0,0 +1,48 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	amaltheadevv1alpha1 "github.com/SwissDataScienceCenter/amalthea/api/v1alpha1"
+)
+
+func needsScaleDown(creationTimestamp metav1.Time, status amaltheadevv1alpha1.AmaltheaSessionStatus, culling amaltheadevv1alpha1.Culling) bool {
+	if status.State == amaltheadevv1alpha1.Hibernated {
+		return false
+	}
+	now := time.Now()
+	age := now.Sub(creationTimestamp.Time)
+	starting := status.State == amaltheadevv1alpha1.NotReady
+	idleSince := status.IdleSince
+	idleDuration := now.Sub(idleSince.Time)
+	failingSince := status.FailingSince
+	failedDuration := now.Sub(failingSince.Time)
+	zero := time.Duration(0)
+	maxAge := culling.MaxAge.Duration
+	maxStartingDuration := culling.MaxStartingDuration.Duration
+	maxIdleDuration := culling.MaxIdleDuration.Duration
+	maxFailedDuration := culling.MaxFailedDuration.Duration
+	return (maxAge > zero && age > maxAge) ||
+		(maxStartingDuration > zero && age > maxStartingDuration && starting) ||
+		(!idleSince.IsZero() && maxIdleDuration > zero && idleDuration > maxIdleDuration) ||
+		(!failingSince.IsZero() && maxFailedDuration > zero && failedDuration > maxFailedDuration)
+}
