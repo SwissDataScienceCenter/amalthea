5e3a64b5907e91333d32bdcfdb586eb695f9abf6
Tasko Olevski <olevski90@gmail.com>
2025-07-04 09:32:40 +0200

    squashme: minor fix

--- api/v1alpha1/amaltheasession_children.go
+++ api/v1alpha1/amaltheasession_children.go
@@ -532,7 +532,8 @@ func (as *AmaltheaSession) DataSources() ([]v1.PersistentVolumeClaim, []v1.Volum
 		case Rclone:
 			storageClass := rcloneStorageClass
 			readOnly := ds.AccessMode == v1.ReadOnlyMany
-			annotations := maps.Clone(as.Spec.Template.Metadata.Annotations)
+			annotations := map[string]string{}
+			maps.Copy(annotations, as.Spec.Template.Metadata.Annotations)
 			annotations[rcloneStorageSecretNameAnnotation] = ds.SecretRef.Name
 			pvcs = append(
 				pvcs,
@@ -960,7 +961,8 @@ func (cr *AmaltheaSession) tunnelContainer() v1.Container {
 }
 
 func (cr *AmaltheaSession) childLabels() map[string]string {
-	labels := maps.Clone(cr.Spec.Template.Metadata.Labels)
+	labels := map[string]string{}
+	maps.Copy(labels, cr.Spec.Template.Metadata.Labels)
 	// NOTE: stuff from selectorLabels will overwrite conflicts in labels (if there are any)
 	// This is the desired behaviour, we do not want to overwrite the selector labels.
 	maps.Copy(labels, selectorLabels(cr.Name))
