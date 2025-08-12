ebb35621644c4293000b4fc2225768f06c220ade
Tasko Olevski <olevski90@gmail.com>
2025-08-12 11:24:41 +0200

    fix: do not try to send request if url is none

--- controller/culling.py
+++ controller/culling.py
@@ -67,7 +67,7 @@ def get_js_server_status(js_body):
         # }
         res = res.json()
     except JSONDecodeError as err:
-        logging.warning(f"Could not parse js server status for {server_url}, because: {err}")
+        logging.warning(f"Could not parse js server status {res.text} for {server_url}, because: {err}")
         return None
 
     if isinstance(res, dict):
