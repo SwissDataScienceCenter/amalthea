4973a4dfc19bbe235c0b5d66bcb0dd2d82f5b229
Samuel Gaist <samuel.gaist@idiap.ch>
2025-09-30 15:00:11 +0200

    refactor: remove Unknown cluster type
    In case of detection failure, it's the error
    that is important not the string value.

--- api/v1alpha1/detection.go
+++ api/v1alpha1/detection.go
@@ -9,17 +9,16 @@ type ClusterType string
 
 const Kubernetes ClusterType = "kubernetes"
 const OpenShift ClusterType = "openshift"
-const UnknownVariant ClusterType = "unknown"
 
 func DetectClusterType(config *rest.Config) (ClusterType, error) {
 	dcl, err := discovery.NewDiscoveryClientForConfig(config)
 	if err != nil {
-		return UnknownVariant, err
+		return "", err
 	}
 
 	apiList, err := dcl.ServerGroups()
 	if err != nil {
-		return UnknownVariant, err
+		return "", err
 	}
 
 	apiGroups := apiList.Groups
