648c58092dc41a0e0bb188308dd62e7511b33740
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-04-22 11:49:43 +0200

    feat: rewrite images (#1112)
    Add a new configuration value `imageRewriteRules` in `helm-chart/amalthea-sessions/values.yaml` which allows session images to be rewritten.

--- internal/controller/amaltheasession_controller.go
+++ internal/controller/amaltheasession_controller.go
@@ -40,6 +40,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	amaltheadevv1alpha1 "github.com/SwissDataScienceCenter/amalthea/api/v1alpha1"
+	"github.com/SwissDataScienceCenter/amalthea/internal/controller/config"
 )
 
 // AmaltheaSessionReconciler reconciles a AmaltheaSession object
@@ -47,7 +48,7 @@ type AmaltheaSessionReconciler struct {
 	client.Client
 	Scheme        *runtime.Scheme
 	MetricsClient metricsv1beta1.PodMetricsesGetter
-	ClusterType   amaltheadevv1alpha1.ClusterType
+	Configuration config.AmaltheaSessionConfiguration
 }
 
 // finalizers
@@ -197,7 +198,7 @@ func (r *AmaltheaSessionReconciler) reconcileInner(ctx context.Context, req ctrl
 		return ctrl.Result{Requeue: true}, nil
 	}
 
-	children, err := NewChildResources(amaltheasession, r.ClusterType)
+	children, err := NewChildResources(amaltheasession, r.Configuration)
 	if err != nil {
 		log.Error(
 			err,
