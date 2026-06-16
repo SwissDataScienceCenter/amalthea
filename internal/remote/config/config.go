a2cd5fe185d20f2caf56b88106f4f9ade2f849b2
Salim Kayal <salim.kayal@idiap.ch>
2026-06-16 15:30:29 +0200

    squashme: remove validation line from bad merge

--- internal/remote/config/config.go
+++ internal/remote/config/config.go
@@ -143,8 +143,7 @@ func GetConfig() (cfg RemoteSessionControllerConfig, err error) {
 }
 
 func (cfg *RemoteSessionControllerConfig) Validate() error {
-	if cfg.ReadinessProbeType != "" &&
-		cfg.ReadinessProbeType != string(amaltheadevv1alpha1.None) &&
+	if cfg.ReadinessProbeType != string(amaltheadevv1alpha1.None) &&
 		cfg.ReadinessProbeType != string(amaltheadevv1alpha1.TCP) &&
 		cfg.ReadinessProbeType != string(amaltheadevv1alpha1.HTTP) {
 		return fmt.Errorf("invalid readiness probe type: %s", cfg.ReadinessProbeType)
