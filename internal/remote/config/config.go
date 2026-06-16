caaa2df6668a75d8f1a3eb9df4d336b78eff5878
Salim Kayal <salim.kayal@idiap.ch>
2026-06-16 15:25:23 +0200

    squashme: remove redefined flag from bad merge

--- internal/remote/config/config.go
+++ internal/remote/config/config.go
@@ -102,7 +102,6 @@ func SetFlags(cmd *cobra.Command) error {
 		return err
 	}
 
-	cmd.Flags().String(readinessProbeTypeFlag, "", "readiness probe type: none, tcp, or http")
 	cmd.Flags().String(readinessProbeTypeFlag, "none", "readiness probe type: none, tcp, or http")
 	if err := viper.BindPFlag(readinessProbeTypeFlag, cmd.Flags().Lookup(readinessProbeTypeFlag)); err != nil {
 		return err
