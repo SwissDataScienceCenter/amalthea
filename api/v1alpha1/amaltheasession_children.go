2f2ab5fc801d792229669f78009fc5b8783eb01a
Lionel Sambuc <lionel.sambuc@epfl.ch>
2026-09-07 16:22:38 +0200

    fix: Address comment, set default value

--- api/v1alpha1/amaltheasession_children.go
+++ api/v1alpha1/amaltheasession_children.go
@@ -684,6 +684,7 @@ func (as *AmaltheaSession) RemoteSessionDataSources() ([]v1.PersistentVolumeClai
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName:  pv.SecretRef.Name,
+							Optional:    ptr.To(false),
 							DefaultMode: ptr.To(int32(0400)), // chmod: r-- --- ---
 						},
 					},
